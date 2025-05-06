b526de0d24389a09ffd9bd2eef32ad6aa2befd84
Klaus Zerwes <zerwes@users.noreply.github.com>
2025-05-06 08:47:21 +0200

    improved startOCSPserver.sh

--- startOCSPserver.sh
+++ startOCSPserver.sh
@@ -5,4 +5,4 @@
 SERVERNAME=ocsp-signing-cert
 
 # the -text option is only for debug
-openssl ocsp -index $CA/index.txt -port 8081 -rsigner $CA/ocsp/$SERVERNAME.pem -rkey $CA/ocsp/$SERVERNAME.key -CA $CA/ca.crt -ignore_err -text #-out log.txt &
+openssl ocsp -index $CA/index.txt -port 8081 -rsigner $CA/ocsp/$SERVERNAME.pem -rkey $CA/ocsp/$SERVERNAME.key -CA $CA/ca.crt -ignore_err -multi 5 -timeout 60 -text  #-out log.txt &
